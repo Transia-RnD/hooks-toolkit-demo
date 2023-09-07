e8329e56261dff950f8675f6b1e310c0bb5f3982
Denis Angell <dangell@transia.co>
2023-09-07 22:25:10 +0200

    Update carbon.c

--- contracts/carbon.c
+++ contracts/carbon.c
@@ -9,8 +9,8 @@ int64_t hook(uint32_t reserved)
 
     uint8_t carbon_accid[20];
     int64_t ret = util_accid(
-            SBUF(carbon_accid),
-            SBUF("rfCarbonVNTuXckX6x2qTMFmFSnm6dEWGX") );
+        SBUF(carbon_accid),
+        SBUF("rfCarbonVNTuXckX6x2qTMFmFSnm6dEWGX"));
     TRACEVAR(ret);
 
     unsigned char hook_accid[20];
@@ -22,7 +22,8 @@ int64_t hook(uint32_t reserved)
     if (account_field_len < 20)
         rollback(SBUF("Carbon: sfAccount field missing!!!"), 1);
 
-    int equal = 0; BUFFER_EQUAL(equal, hook_accid, account_field, 20);
+    int equal = 0;
+    BUFFER_EQUAL(equal, hook_accid, account_field, 20);
     if (!equal)
     {
         accept(SBUF("Carbon: Incoming transaction"), 2);
@@ -32,16 +33,16 @@ int64_t hook(uint32_t reserved)
     int64_t amount_len = otxn_field(SBUF(amount_buffer), sfAmount);
     int64_t drops_to_send = 1000; // this will be the default
 
-
     if (amount_len != 8)
     {
         TRACESTR("Carbon: Non-xrp transaction detected, sending default 1000 drops to rfCarbon");
-    } else
+    }
+    else
     {
         TRACESTR("Carbon: XRP transaction detected, computing 1% to send to rfCarbon");
         int64_t otxn_drops = AMOUNT_TO_DROPS(amount_buffer);
         TRACEVAR(otxn_drops);
-        if (otxn_drops > 100000)   // if its less we send the default amount. or if there was an error we send default
+        if (otxn_drops > 100000)                                   // if its less we send the default amount. or if there was an error we send default
             drops_to_send = (int64_t)((double)otxn_drops * 0.01f); // otherwise we send 1%
     }
 
@@ -50,12 +51,11 @@ int64_t hook(uint32_t reserved)
     unsigned char tx[PREPARE_PAYMENT_SIMPLE_SIZE];
 
     PREPARE_PAYMENT_SIMPLE(tx, drops_to_send, carbon_accid, 0, 0);
-    
+
     uint8_t emithash[32];
     int64_t emit_result = emit(SBUF(emithash), SBUF(tx));
     TRACEVAR(emit_result);
 
     accept(SBUF("Carbon: Emitted transaction"), 0);
     return 0;
-
 }
